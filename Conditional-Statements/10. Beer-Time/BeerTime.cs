9195a11d44aa17f10bf05891c41446124f7a98b8
Stoyan Grigorov <hacker4eto@gmail.com>
2015-11-11 19:12:15 +0100

    hwupd

---
+++ Conditional-Statements/10. Beer-Time/BeerTime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+/*
+10.* Beer Time
+
+A beer time is after 1:00 PM and before 3:00 AM.
+Write a program that enters a time in format “hh:mm tt” 
+(an hour in range [01...12], a minute in range [00…59] and AM / PM designator)
+and prints beer time or non-beer time according to the definition above or invalid time if the time cannot be parsed.
+Note: You may need to learn how to parse dates and times.
+*/
+class BeerTime
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Enter the time you wish to check in the following format \"hh:mm tt\", \nExample 1: 1:14 am\nExample 2: 11:45 pm");
+            string time = Console.ReadLine();
+            DateTime usertime = DateTime.Parse(time);
+            usertime.ToString("hh:mm tt");
+            DateTime startTime = DateTime.Parse("01:00 PM");
+            DateTime endTime = DateTime.Parse("03:00 AM");
+            if (usertime >= startTime || usertime < endTime)
+            {
+                Console.WriteLine("Its beer time!!!");
+            }
+            else
+            {
+                Console.WriteLine("Not beer time, yet ;(");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input, try again.");
+            Main();
+        }
+    }
+}
---
+++ Conditional-Statements/9. Play-with-Int,-Double-and-String/IntDoubleString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+/*
+9. Play with Int, Double and String
+
+Write a program that, depending on the user’s choice, inputs an int, double or string variable.
+If the variable is int or double, the program increases it by one.
+If the variable is a string, the program appends * at the end.
+Print the result at the console. Use switch statement.
+*/
+class IntDoubleString
+{
+    static void Main()
+    {
+        Console.WriteLine("Please choose a type:\n\nInt --> 1\nDouble --> 2\nString --> 3\n");
+        int decision = int.Parse(Console.ReadLine());
+        switch (decision)
+        {
+            case 1:
+                Console.WriteLine("Please enter a integer:");
+                int intValue = int.Parse(Console.ReadLine());
+                Console.WriteLine("Result: {0}",intValue + 1);
+                break;
+            case 2:
+                Console.WriteLine("Please enter a double:");
+                double doubleValue = double.Parse(Console.ReadLine());
+                Console.WriteLine("Result: {0}", doubleValue + 1);
+                break;
+            case 3:
+                Console.WriteLine("Please enter a string:");
+                string stringValue = Console.ReadLine();
+                Console.WriteLine("Result: {0}", stringValue + "*");
+                break;
+            default:
+                Console.WriteLine("Invalid choice input.");
+                break;
+        }
+
+    }
+}
