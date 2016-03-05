31a8eb1b6745348190c142a402b3cfbb6aecb6e8
Stoyan Grigorov <hacker4eto@gmail.com>
2016-03-05 11:16:36 +0100

    Commit Variant 1
    Variant 1 completed with 2 solutions to problem 5

---
+++ Variant 1 Exam/Problem 5. Neurons v2/NeuronsV2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+Problem 5. Neurons
+*/
+class NeuronsSecondVersion
+{
+    static void Main()
+    {
+        List<long> results = new List<long>();
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "-1")
+            {
+                for (int j = 0; j < results.Count; j++)
+                {
+                    Console.WriteLine(results[j]);
+                }
+                break;
+            }
+            long inputNumber = long.Parse(input);
+            if (inputNumber == 0)
+            {
+                results.Add(inputNumber);
+                continue;
+            }
+            int mostRightBorder = -1;
+            int mostLeftBorder = -1;
+            for (int p = 0; p < 32; p++)
+            {
+                long mask = 1 << p;
+                long numberAndMask = inputNumber & mask;
+                long bit = numberAndMask >> p;
+
+                if (bit == 1)
+                {
+                    if (mostRightBorder == -1)
+                    {
+                        mostRightBorder = p;
+                    }
+                    mostLeftBorder = p;
+                }
+            }
+            long result = 0;
+            for (int p = mostRightBorder; p <= mostLeftBorder; p++)
+            {
+                long mask = 1 << p;
+                long numberAndMask = inputNumber & mask;
+                long bit = numberAndMask >> p;
+                if (bit == 0)
+                {
+                    result = result | mask;
+                }
+            }
+            results.Add(result);
+        }
+    }
+}
---
+++ Variant 1 Exam/Problem 5. Neurons/Neurons.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+Problem 5. Neurons
+*/
+class Neurons
+{
+    static void Main()
+    {
+        //get the numbers
+        bool inputEnabler = true;
+        List<Int64> numbers = new List<Int64>();
+        while (inputEnabler)
+        {
+            Int64 input = Int64.Parse(Console.ReadLine());
+            if (input == -1)
+            {
+                inputEnabler = false;
+            }
+            else
+            {
+                numbers.Add(input);
+            }
+        }
+        //Calculating the results and storing them
+        List<string> results = new List<string>();
+        string[] numbersArray = new string[numbers.Count];
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            numbers[i] = ~numbers[i];
+            numbersArray[i] = Convert.ToString(numbers[i], 2);
+            int strLStart = numbersArray[i].Length;
+            numbersArray[i] = numbersArray[i].TrimEnd('1');
+            int strLEnd = strLStart - numbersArray[i].Length;
+            numbersArray[i] = numbersArray[i].TrimStart('1');
+            strLStart = numbersArray[i].Length;
+            numbersArray[i] = numbersArray[i].TrimEnd('0');
+            int strMEnd = strLStart - numbersArray[i].Length;
+            strLEnd += strMEnd;
+            numbersArray[i] = numbersArray[i].TrimStart('0');
+            results.Add(numbersArray[i]);
+            if (results[i] == "")
+            {
+                results[i] = "0";
+            }
+            else
+            {
+                for (int j = 0; j < strLEnd; j++)
+                {
+                    results[i] += "0";
+                }
+
+            }
+        }
+        //Print the results
+        foreach (String item in results)
+        {
+            Int64 tempNumber = Convert.ToInt64(item, 2);
+            Console.WriteLine(tempNumber);
+        }
+    }
+}
